1160dbbceaa754fe42b10711fa54272fa40c2079
MercurialMune <munenecyp@gmail.com>
2019-01-11 12:45:00 +0300

    create quote component create quote details component add bootstrap add awesomeFonts add template backgrounds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent'
+    AngularFontAwesomeMudule
   ],
   imports: [
     BrowserModule,
